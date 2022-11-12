7fcdb3796d652a3a698480cf74a3d6c1f2273183
nando-castro <fernando.castro10.master@gmail.com>
2022-11-12 00:45:28 -0300

    fix: remove function

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,11 +1,9 @@
-import { getUser, signIn, signUp } from "../controllers/authController.js";
+import { signIn, signUp } from "../controllers/authController.js";
 import { Router } from 'express';
 
 const router = Router();
 
 router.post("/sign-up", signUp);
 router.post("/sign-in", signIn);
-
-router.get("/sign-in", getUser);
 
 export default router;
